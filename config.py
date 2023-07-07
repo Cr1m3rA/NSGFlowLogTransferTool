9253d220f17cc8cd33ada8f1d2e87edb57938864
Cr1m3rA <39370444+Cr1m3rA@users.noreply.github.com>
2023-07-07 09:43:33 +0800

    Create config.py

---
+++ config.py
@@ -0,0 +1,10 @@
+# config.py
+# Azure connection string  
+connection_string = "*****************************"
+
+# Contrainer name
+container_name = "***"
+# Azure Monitor Configuration
+customer_id = '*************'
+shared_key = "***********"
+log_type = '*********'
